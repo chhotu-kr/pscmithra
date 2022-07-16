c4b584c2f18162dd6956f03162dfe21f7422ca72
officialpiyush1 <officalpiyush81@gmail.com>
2022-07-16 11:04:55 +0530

    new

--- database/migrations/2022_07_13_060415_create_studymetrial_categories_table.php
+++ database/migrations/2022_07_13_060415_create_studymetrial_categories_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('studymetrial_categories', function (Blueprint $table) {
+        Schema::create('smcategories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('image');
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('studymetrial_categories');
+        Schema::dropIfExists('smcategories');
     }
 };
