045a64933955bc9af7ff61025e2839903b364b6a
officialpiyush1 <officalpiyush81@gmail.com>
2022-08-09 10:10:02 +0530

    ss

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -18,36 +18,36 @@ class CategorySeeder extends Seeder
     public function run()
     {      //
         $category = [
-            ['id' => '1','category' => 'Banking','slugid'=>'wer434fd',],
-            ['id' => '2','category' => 'Railway','slugid'=>'werg55634',],
-            ['id' => '3','category'=>'SSC','slugid'=>'wesfr0fd74',],
-            ['id' => '4','category'=>'MTS','slugid'=>'wesfr0fd34',],
-            ['id' => '5','category'=>'NEET','slugid'=>'wesfr2erd34',],
-            ['id' => '6','category'=>'UPSC','slugid'=>'wesfr2erd3j',],
-            ['id' => '7','category'=>'UPSC NDA','slugid'=>'435sfr2erd34',],
-            ['id' => '8','category'=>'IBPS','slugid'=>'435sfr2erda4',],
+            ['id' => '1','category' => 'Banking','slugid'=>'wer434fd','image'=>'python.jpg'],
+            ['id' => '2','category' => 'Railway','slugid'=>'werg55634','image'=>'django.jpg'],
+            ['id' => '3','category'=>'SSC','slugid'=>'wesfr0fd74','image'=>'laravel.jpg'],
+            ['id' => '4','category'=>'MTS','slugid'=>'wesfr0fd34','image'=>'django.jpg'],
+            ['id' => '5','category'=>'NEET','slugid'=>'wesfr2erd34','image'=>'python.jpg'],
+            ['id' => '6','category'=>'UPSC','slugid'=>'wesfr2erd3j','image'=>'laravel.jpg'],
+            ['id' => '7','category'=>'UPSC NDA','slugid'=>'435sfr2erd34','image'=>'python.jpg'],
+            ['id' => '8','category'=>'IBPS','slugid'=>'435sfr2erda4','image'=>'django.jpg'],
            
             
          ];
          Category::insert($category);
 
         $subcategory = [
-            ['id' => '1','category_id'=>'1','subcategory' => 'BankPO','slugid'=>'we00434fd',],
-            ['id' => '2','category_id'=>'1','subcategory' => 'Clerk','slugid'=>'werg1q634',],
-            ['id' => '3','category_id'=>'2','subcategory' => 'TT','slugid'=>'wec44fd',],
-            ['id' => '4','category_id'=>'2','subcategory' => 'Junior Manager','slugid'=>'wer4b5634',],
-            ['id' => '5','category_id' => '3','subcategory'=>'Assistent Officer','slugid'=>'wesqzfd74',],
-            ['id' => '6','category_id' => '3','subcategory'=>'Sub Inspector','slugid'=>'wesf90hd34',],
-            ['id' => '7','category_id' => '4','subcategory'=>'Chowkidar','slugid'=>'wesvnmfd74',],
-            ['id' => '8','category_id' => '4','subcategory'=>'Junior Gestetner Operator','slugid'=>'wesxsdfd34',],
-            ['id' => '9','category_id' => '5','subcategory'=>'MBBS','slugid'=>'wesfanmrd34',],
-            ['id' => '10','category_id' => '5','subcategory'=>'Physian','slugid'=>'wesf1zbrd3j',],
-            ['id' => '11','category_id' => '6','subcategory'=>'Cardologist','slugid'=>'westrerd34',],
-            ['id' => '12','category_id' => '6','subcategory'=>'MS','slugid'=>'wes58rd3j',],
-            ['id' => '13','category_id' => '7','subcategory'=>'DM','slugid'=>'435synrd34',],
-            ['id' => '14','category_id' => '7','subcategory'=>'BDO','slugid'=>'435fh1rda4',],
-            ['id' => '15','category_id' => '8','subcategory'=>'	UCO Bank ','slugid'=>'435sedc2erda4',],
-            ['id' => '16','category_id' => '8','subcategory'=>'	Indian Bank ','slugid'=>'43wq1erda4',],
+            ['id' => '1','category_id'=>'1','subcategory' => 'BankPO','slugid'=>'we00434fd','image'=>'django.jpg'],
+            ['id' => '2','category_id'=>'1','subcategory' => 'Clerk','slugid'=>'werg1q634','image'=>'laravel.jpg'],
+            ['id' => '3','category_id'=>'2','subcategory' => 'TT','slugid'=>'wec44fd','image'=>'django.jpg'],
+            ['id' => '4','category_id'=>'2','subcategory' => 'Junior Manager','slugid'=>'wer4b5634','image'=>'python.jpg'],
+            ['id' => '5','category_id' => '3','subcategory'=>'Assistent Officer','slugid'=>'wesqzfd74','image'=>'python.jpg'],
+            ['id' => '6','category_id' => '3','subcategory'=>'Sub Inspector','slugid'=>'wesf90hd34','image'=>'laravel.jpg'],
+            ['id' => '7','category_id' => '4','subcategory'=>'Chowkidar','slugid'=>'wesvnmfd74','image'=>'laravel.jpg'],
+            ['id' => '8','category_id' => '4','subcategory'=>'Junior Gestetner Operator','slugid'=>'wesxsdfd34','image'=>'django.jpg'],
+            ['id' => '9','category_id' => '5','subcategory'=>'MBBS','slugid'=>'wesfanmrd34','image'=>'django.jpg'],
+            ['id' => '10','category_id' => '5','subcategory'=>'Physian','slugid'=>'wesf1zbrd3j','image'=>'laravel.jpg'],
+            ['id' => '11','category_id' => '6','subcategory'=>'Cardologist','slugid'=>'westrerd34','image'=>'django.jpg'],
+            ['id' => '12','category_id' => '6','subcategory'=>'MS','slugid'=>'wes58rd3j','image'=>'laravel.jpg'],
+            ['id' => '13','category_id' => '7','subcategory'=>'DM','slugid'=>'435synrd34','image'=>'django.jpg'],
+            ['id' => '14','category_id' => '7','subcategory'=>'BDO','slugid'=>'435fh1rda4','image'=>'laravel.jpg'],
+            ['id' => '15','category_id' => '8','subcategory'=>'	UCO Bank ','slugid'=>'435sedc2erda4','image'=>'laravel.jpg'],
+            ['id' => '16','category_id' => '8','subcategory'=>'	Indian Bank ','slugid'=>'43wq1erda4','image'=>'django.jpg'],
            
             
          ];
