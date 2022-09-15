1f116b70d17c928a3d4702afd3d72b4df46fbc85
Uttsav <uttsavkumar85410@gmail.com>
2022-09-15 14:42:20 +0530

    new commit

--- app/Models/userPdf.php
+++ app/Models/userPdf.php
@@ -5,7 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class userPdf extends Model
+class UserPdf extends Model
 {
     use HasFactory;
+
+    public function sessionpdf(){
+        return $this->hasMany(Pdf::class,'pdf_id','id');
+    }
+
+    public function currentuser(){
+        return $this->hasOne(User::class,'id','user_id');
+    }
+
+    public function product(){
+        return $this->hasMany(Product::class,'product_id','id');
+    }
+
+    public function order(){
+        return $this->hasOne(Order::class,'id','order_id');
+    }
 }
