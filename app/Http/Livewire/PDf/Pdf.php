ae39551e477c9c6e5c079c00967cd5cc4793017e
chhotu kumar <ck5823858@gmail.com>
2022-09-16 17:44:09 +0100

    newwwwwww

--- app/Http/Livewire/PDf/Pdf.php
+++ app/Http/Livewire/PDf/Pdf.php
@@ -10,26 +10,7 @@ class Pdf extends Component
     public $data=[];
 
     public function mount(){
-        // if (empty($request->user)) {
-        //     return response()->json(['msg' => 'Enter User', 'status' => false]);
-        //   }
-        //   $user_id =  User::where("slugid", $request->user)->first();
-        //   if (!$user_id) {
-        //     return response()->json(['msg' => 'Invalid User ID', 'status' => false]);
-        //   }
       
-      
-        //   $data  =  userPdf::where("user_id", $user_id->id)->with('pdf')->get();
-        //   if (count($data) == 0) {
-        //     return response()->json(['msg' => 'Data Fetched', 'status' => true, 'data' => $data]);
-        //   }
-        //   $data = $data->map(function ($item) {
-        //     return [
-        //       "pdf" => $item->pdf->pdf_url,
-        //       "name" => $item->pdf->name,
-        //     ];
-        //   });
-        //   return response()->json(['msg' => 'Data Fetched', 'status' => true, 'data' => $data]);
 
         $user_id=Auth::id();
         $data  =  userPdf::where("user_id", $user_id)->with('pdf')->get();
